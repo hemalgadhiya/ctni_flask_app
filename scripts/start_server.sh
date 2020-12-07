bbf5bbb50da0a1954608734c210265204cd42245
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-12-07 17:34:00 -0500

    added full path to start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 cd /home/ubuntu/ctni
-source /venv/bin/activate
+pwd
+source /home/ubuntu/ctni/venv/bin/activate
 nohup gunicorn --bind 0.0.0.0:8000 wsgi > /home/ubuntu/output.txt 2> /home/ubuntu/erro.txt &
